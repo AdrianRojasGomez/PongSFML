d9f818e449025bea56c0e6ff43a5ca41246e055e
Adrian Rojas <arojasgomez.93@gmail.com>
2024-11-24 10:03:29 -0300

    feat: add player input, hud and draw

--- PongSFML/src/Main.cpp
+++ PongSFML/src/Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <sstream>
 #include "Entities/Bat.h"
 
 int main()
@@ -15,7 +16,67 @@ int main()
     sf::Font font;
     
     if (font.loadFromFile("res/fonts/VT323.ttf"))
+    {
         hud.setFont(font);
+        hud.setCharacterSize(75);
+        hud.setFillColor(sf::Color::White);
+        hud.setPosition(20, 20);
+    }
     else
-        std::cout << "ERROR, LOADING FONT " << font << ".";
+        std::cout << "ERROR, LOADING FONT";
+
+    sf::Clock clock;
+
+    while (window.isOpen())
+    {
+        //Handle player input
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+            {
+                window.close();
+            }
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+                bat.MoveLeft();
+            else
+                bat.StopLeft();
+
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+                bat.MoveRight();
+            else
+                bat.StopRight();
+
+        }
+
+
+
+
+
+        //update elements
+        sf::Time dt = clock.restart();
+        bat.Update(dt);
+
+        std::stringstream ss;
+        ss << "Score:" << score << " Lives:" << lives;
+        hud.setString(ss.str());
+
+
+
+
+        //draw elements
+        window.clear();
+        window.draw(hud);
+        window.draw(bat.GetShape());
+        window.display();
+    }
+
+    return 0;
+
 }
