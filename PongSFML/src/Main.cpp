fb4b83ac7e5bff1dad93c90b9d5f79451aaab69d
Adrian Rojas <arojasgomez.93@gmail.com>
2024-11-24 09:46:18 -0300

    feat: add new view and font

--- PongSFML/src/Main.cpp
+++ PongSFML/src/Main.cpp
@@ -1,6 +1,21 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include "Entities/Bat.h"
 
 int main()
 {
+    sf::VideoMode vm(1920, 1080);
+    sf::RenderWindow window(vm, "Pong", sf::Style::Fullscreen);
+    int score = 0;
+    int lives = 3;
+
+    Bat bat(1920 / 2, 1080 - 20);
+
+    sf::Text hud;
+    sf::Font font;
     
+    if (font.loadFromFile("res/fonts/VT323.ttf"))
+        hud.setFont(font);
+    else
+        std::cout << "ERROR, LOADING FONT " << font << ".";
 }
