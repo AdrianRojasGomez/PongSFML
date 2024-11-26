e42a7494f0f81593b46d04796b2ced98d81ca282
Adrian Rojas <arojasgomez.93@gmail.com>
2024-11-26 07:05:32 -0300

    Create Ball Class

---
+++ PongSFML/Ball.cpp
@@ -0,0 +1,32 @@
+#include "Ball.h"
+
+Ball::Ball(float startX, float startY) : m_Position(startX, startY)
+{
+	m_Shape.setSize(sf::Vector2f(10, 10));
+	m_Shape.setPosition(m_Position);
+}
+
+void Ball::ReboundSides()
+{
+	m_DirectionX = -m_DirectionX;
+}
+
+void Ball::ReboundBatOrTop()
+{
+	m_DirectionY = -m_DirectionY;
+}
+
+void Ball::ReboundBottom()
+{
+	m_Position.y = 0;
+	m_Position.x = 500;
+	m_DirectionX = -m_DirectionY;
+}
+
+void Ball::Update(sf::Time dt)
+{
+	m_Position.x += m_DirectionX * m_Speed * dt.asSeconds();
+	m_Position.y += m_DirectionY * m_Speed * dt.asSeconds();
+
+	m_Shape.setPosition(m_Position);
+}
---
+++ PongSFML/Ball.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Ball
+{
+public:
+	Ball(float startX, float startY);
+	~Ball();
+	sf::FloatRect GetPosition() { return m_Shape.getGlobalBounds(); }
+	sf::RectangleShape GetShape() { return m_Shape; }
+	float GetXVelocity() { return m_DirectionX; }
+	void ReboundSides();
+	void ReboundBatOrTop();
+	void ReboundBottom();
+	void Update(sf::Time dt);
+
+private:
+	sf::Vector2f m_Position;
+	sf::RectangleShape m_Shape;
+	float m_Speed = 300.0f;
+	float m_DirectionX = 0.2f;
+	float m_DirectionY = 0.2f;
+};
